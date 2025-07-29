d8675985cfe1f88b03bdd7ec80af600a7af22883
Zain Sarfraz <zainsarfraz82@gmail.com>
2025-07-29 12:56:08 +0500

    Updated

---
+++ crud/app.mjs
@@ -0,0 +1,28 @@
+import dotenv from "dotenv"
+import express from "express"
+import router from "./routes/route.mjs"
+import mongoose from "mongoose"
+import cors from "cors"
+
+
+const app = express()
+//body parser
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+const port = process.env.PORT
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_CONNECTION_URL);
+  console.log("db connected")
+}
+
+
+app.use("/",router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
